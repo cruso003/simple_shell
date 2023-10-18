67b0ebaa633659402ab350e246970c1fce6e59b7
Geitodyu Henrique Crusoe <crusoehenrique3@gmail.com>
2023-10-19 01:17:12 +0300

    betty error fixed

--- _strcpy.c
+++ _strcpy.c
@@ -9,14 +9,14 @@
  */
 char *_strcpy(char *dest, const char *src)
 {
-    int i = 0;
+	int i = 0;
 
-    while (src[i] != '\0')
-    {
-        dest[i] = src[i];
-        i++;
-    }
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
 
-    dest[i] = '\0';
-    return (dest);
+	dest[i] = '\0';
+	return (dest);
 }
