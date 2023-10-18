3de8dc85ae2f7f199b462f5656bf2be3fcbdb0ea
ElizabethAgada <lizzyegbo2015@gmail.com>
2023-10-18 10:31:02 -0700

    prompt.c

---
+++ prompt.c
@@ -0,0 +1,25 @@
+#include "shell.h"
+/**
+ * get_user_input - get the user input from the line
+ * @lineptr: the line pointer to get the user input from
+ * the line
+ * @n: the number of characters to get from the line
+ * Return: the number of characters received
+ */
+ssize_t get_user_input(char **lineptr, size_t *n)
+{
+	char *prompt = "$ ";
+	size_t read;
+
+	if (write(1, prompt, _strlen(prompt)) == -1)
+	{
+		perror("write error");
+		return (-1);
+	}
+	read = custom_getline(lineptr, n);
+	if (!read)
+	{
+		return (-1);
+	}
+	return (read);
+}
