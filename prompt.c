4d4acf2ed1d70b2fbb147f6dae0f548138135c40
Geitodyu Henrique Crusoe <crusoehenrique3@gmail.com>
2023-10-19 04:21:01 +0300

    fixed 2

--- prompt.c
+++ prompt.c
@@ -11,12 +11,12 @@ ssize_t get_user_input(char **lineptr, size_t *n)
 	char *prompt = "$ ";
 	size_t read;
 
-	if (write(1, prompt, _strlen(prompt)) == -1)
+	if (write(1, prompt, strlen(prompt)) == -1)
 	{
 		perror("write error");
 		return (-1);
 	}
-	read = custom_getline(lineptr, n);
+	read = getline(lineptr, n);
 	if (!read)
 	{
 		return (-1);
