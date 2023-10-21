000e86092ec48002089553ea272d83aefe6a624f
Geitodyu Henrique Crusoe <crusoehenrique3@gmail.com>
2023-10-21 15:25:10 +0300

    test

--- builtin.c
+++ builtin.c
@@ -19,7 +19,7 @@ int handle_builtin_commands(char **tokens)
 	{
 		if (tokens[1] != NULL)
 		{
-			int status = strtol(tokens[1], &endptr, 10);
+			long status = strtol(tokens[1], &endptr, 10);
 
 			if (*endptr != '\0')
 			{
@@ -27,7 +27,7 @@ int handle_builtin_commands(char **tokens)
 			}
 			else
 			{
-				exit(status);
+				exit((int)status);
 			}
 		}
 		return (1);
